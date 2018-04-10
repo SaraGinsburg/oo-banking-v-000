e9e6c01cbdd89f1df30a33e140008e0937024ed6
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-04-10 22:09:58 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -9,27 +9,3 @@ class BankAccount
     @status = "open"
   end
 end
-
-# let(:avi) { BankAccount.new("Avi") }
-
-# describe "#initialize" do
-#   it "can initialize a Bank Account" do
-#     expect(avi).to be_a(BankAccount)
-#   end
-#
-#   it "initializes with a name" do
-#     expect(avi.name).to eq("Avi")
-#   end
-#
-#   it "always initializes with balance of 1000" do
-#     expect(avi.balance).to eq(1000)
-#   end
-#
-#   it "always initializes with a status of 'open'" do
-#     expect(avi.status).to eq("open")
-#   end
-#
-#   it "can't change its name" do
-#     expect { avi.name = "Bob" }.to raise_error
-#   end
-# end
