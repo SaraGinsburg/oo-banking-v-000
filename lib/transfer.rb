a8dce2bacf800062db06e2356ab2af2448da5176
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-04-10 22:31:38 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,22 +12,39 @@ class Transfer
   def valid?
     @sender.valid? && @receiver.valid?
   end
+
+  def execute_transaction
+    if self.valid?
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+  end
+
+  # describe '#execute_transaction' do
+  #   it "can execute a successful transaction between two accounts" do
+  #     transfer.execute_transaction
+  #     expect(amanda.balance).to eq(950)
+  #     expect(avi.balance).to eq(1050)
+  #     expect(transfer.status).to eq("complete")
+  #   end
+  #
+  #   it "each transfer can only happen once" do
+  #     transfer.execute_transaction
+  #     expect(amanda.balance).to eq(950)
+  #     expect(avi.balance).to eq(1050)
+  #     expect(transfer.status).to eq("complete")
+  #     transfer.execute_transaction
+  #     expect(amanda.balance).to eq(950)
+  #     expect(avi.balance).to eq(1050)
+  #   end
+  #
+  #   it "rejects a transfer if the sender doesn't have a valid account" do
+  #     expect(bad_transfer.execute_transaction).to eq("Transaction rejected. Please check your account balance.")
+  #     expect(bad_transfer.status).to eq("rejected")
+  #   end
+  # end
+
 end
-
-
-# describe '#valid?' do
-#   it "can check that both accounts are valid" do
-#     expect(avi.valid?).to eq (true)
-#     expect(amanda.valid?).to eq(true)
-#     expect(transfer.valid?).to eq(true)
-#   end
-#
-#   it "calls on the sender and reciever's #valid? methods" do
-#     transfer_class = File.read("lib/transfer.rb")
-#
-#     expect(amanda).to receive(:valid?).and_return(true)
-#     expect(avi).to receive(:valid?).and_return(true)
-#
-#     transfer.valid?
-#   end
-# end
