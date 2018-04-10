d5024055eba8b00f21086cf8d091b593df87f466
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-04-10 22:43:38 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,13 +14,15 @@ class Transfer
   end
 
   def execute_transaction
-    if self.valid? 
-      @sender.balance -= @amount
-      @receiver.balance += @amount
-      @status = "complete"
-    else
-      @status = "rejected"
-      "Transaction rejected. Please check your account balance."
+    unless @status == "complete"
+      if self.valid?
+        @sender.balance -= @amount
+        @receiver.balance += @amount
+        @status = "complete"
+      else
+        @status = "rejected"
+        "Transaction rejected. Please check your account balance."
+      end
     end
   end
 
