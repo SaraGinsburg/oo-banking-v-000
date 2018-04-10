0a05680d288f05eb44afd0845f6aaf9ffbcc0028
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-04-10 22:21:28 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,37 @@
 class Transfer
-  # your code here
+  attr_accessor :status
+  attr_reader :sender, :receiver, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
 end
+
+# let(:transfer) { Transfer.new(amanda, avi, 50) }
+# let(:bad_transfer) { Transfer.new(amanda, avi, 4000) }
+#
+# describe 'Transfer' do
+#   describe 'initialize' do
+#     it "can initialize a Transfer" do
+#       expect(transfer).to be_a(Transfer)
+#     end
+#
+#     it "initializes with a sender" do
+#       expect(transfer.sender).to eq(amanda)
+#     end
+#
+#     it "initializes with a receiver" do
+#       expect(transfer.receiver).to eq(avi)
+#     end
+#
+#     it "always initializes with a status of 'pending'" do
+#       expect(transfer.status).to eq("pending")
+#     end
+#
+#     it "initializes with a transfer amount" do
+#       expect(transfer.amount).to eq(50)
+#     end
+#   end
