03481502ec2b549e4ee2a006829b90a3bfbead21
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-04-10 22:12:11 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,11 +1,15 @@
 class BankAccount
   attr_accessor :balance, :status
-  attr_reader :name
+  attr_reader :name, :deposit
 
 
   def initialize(name)
     @name = name.capitalize
     @balance = 1000
     @status = "open"
+  end
+
+  def deposit(amount)
+    @balance += amount
   end
 end
